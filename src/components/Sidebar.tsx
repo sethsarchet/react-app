fc5f41e9efb71e12f4fd53e1652b6c48f601b662
Seth Sarchet <smspeed99@hotmail.com>
2023-07-11 16:40:27 -0700

    Got the app working locally

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
 import { styled } from '@mui/joy/styles';
 import GlobalStyles from '@mui/joy/GlobalStyles';
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/joy/styles';
 import App from './App';
 
-ReactDOM.createRoot(document.querySelector("#root")).render(
-  <React.StrictMode>
-    <StyledEngineProvider injectFirst>
-      <App />
-    </StyledEngineProvider>
-  </React.StrictMode>
-);
+const root = document.querySelector("#root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <StyledEngineProvider injectFirst>
+        <App />
+      </StyledEngineProvider>
+    </React.StrictMode>
+  );
+} else {
+  console.error("The root element could not be found");
+}
